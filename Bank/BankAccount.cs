b162c83a0a7ca8a06e82fb33e3976725aeec46b6
BakiDag <73464615+BakiDag@users.noreply.github.com>
2022-10-03 19:23:36 +0200

    Add project files.

---
+++ Bank/BankAccount.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Bank
+{
+    /// <summary>
+    /// Bank account demo class.
+    /// </summary>
+    public class BankAccount
+    {
+        private double balance;
+
+        public BankAccount()
+        {
+        }
+
+        public BankAccount(double balance)
+        {
+            this.balance = balance;
+        }
+
+        public double Balance
+        {
+            get { return balance; }
+        }
+
+        public void Add(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount));
+            }
+
+            balance += amount;
+        }
+
+        //abheben
+        public void Withdraw(double amount)
+        {
+            if (amount > balance)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount));
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount));
+            }
+
+            balance -= amount;
+        }
+
+        //transfer from current acc to any other acc
+        public void TransferFundsTo(BankAccount otherAccount, double amount)
+        {
+            if (otherAccount is null)
+            {
+                throw new ArgumentNullException(nameof(otherAccount));
+            }
+
+            Withdraw(amount);
+            otherAccount.Add(amount);
+        }
+    }
+}
---
+++ BankXUnitTests/BankAccountTests.cs
@@ -0,0 +1,73 @@
+using Bank;
+using System;
+using Xunit;
+
+namespace BankXUnitTests
+{
+    public class BankAccountTests
+    {
+        [Fact]
+        public void Adding_Funds_Updates_Balance()
+        {
+            //Arrange
+            BankAccount account = new BankAccount(1000);
+
+            //Act
+            account.Add(100);
+
+            //Assert
+            Assert.Equal(1100, account.Balance);
+        }
+
+        [Fact]
+        public void Adding_Negative_Funds_Throws()
+        {
+            //Arrange
+            BankAccount account = new BankAccount(1000);
+
+            //Act +Assert            
+            Assert.Throws<ArgumentOutOfRangeException>(() => account.Add(-100));
+        }
+
+        [Fact]
+        public void Withdrawing_Funds_Updates_Balance()
+        {
+            //Arrange
+            BankAccount account = new BankAccount(1000);
+
+            //Act
+            account.Withdraw(100);
+
+            //Assert
+            Assert.Equal(900, account.Balance);
+        }
+
+        [Fact]
+        public void Withdrawing_Negative_Funds_Updates_Balance()
+        {
+            //Arrange
+            BankAccount account = new BankAccount(1000);
+
+            //Act
+            account.Withdraw(100);
+
+            //Assert
+            Assert.Equal(900, account.Balance);
+        }
+
+        [Fact]
+        public void Transfering_Funds_Updates_Both_Accounts()
+        {
+            //Arrange
+            BankAccount account = new BankAccount(1000);
+            BankAccount otherAccount = new BankAccount();
+
+            //Act
+            account.TransferFundsTo(otherAccount, 500);
+
+            //Assert
+            Assert.Equal(500, account.Balance);
+            Assert.Equal(500, otherAccount.Balance);
+        }
+    }
+}
